4370ca69b7f7764ce9e4127b3b9ef59aaa645b10
Armin Krezović <armin.krezovic@fet.ba>
2016-11-12 01:59:53 +0100

    Implementiran stack trace.

--- kern/monitor.c
+++ kern/monitor.c
@@ -57,7 +57,11 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+	cprintf("Stack backtrace:\n");
+
+	for (uint32_t *ebp = (uint32_t *) read_ebp(); ebp != 0; ebp = (uint32_t *) ebp[0])
+		cprintf("ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n", (uint32_t) ebp, ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+
 	return 0;
 }
 
