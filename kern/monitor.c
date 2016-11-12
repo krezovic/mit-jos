715b388011f937daf51d9aec552efd9feea32e9d
Armin Krezović <armin.krezovic@fet.ba>
2016-11-12 02:01:13 +0100

    Implementiran ispis imena funkcija unutar stack trace

--- kern/monitor.c
+++ kern/monitor.c
@@ -59,8 +59,14 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	cprintf("Stack backtrace:\n");
 
-	for (uint32_t *ebp = (uint32_t *) read_ebp(); ebp != 0; ebp = (uint32_t *) ebp[0])
+	for (uint32_t *ebp = (uint32_t *) read_ebp(); ebp != 0; ebp = (uint32_t *) ebp[0]) {
+		struct Eipdebuginfo info;
+
+		debuginfo_eip(ebp[1], &info);
+
 		cprintf("ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n", (uint32_t) ebp, ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+		cprintf("       %s:%d: %.*s+%d\n", info.eip_file, info.eip_line, info.eip_fn_namelen, info.eip_fn_name, ebp[1] - info.eip_fn_addr);
+	}
 
 	return 0;
 }
